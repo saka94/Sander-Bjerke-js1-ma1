18136cf88240c49f2c6c84618340b93bda76ac66
saka94 <74228457+saka94@users.noreply.github.com>
2021-03-28 06:18:12 +0200

    i

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+// question 1
+
+function cat(complain) {
+    console.log(complain)
+}
+cat("Meow!")
